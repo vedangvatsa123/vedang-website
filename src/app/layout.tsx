b03ae669c1eac63e7623206d5675723f37bc1e21
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-12-02 17:59:07 +0000

    still not solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from 'next';
-import '@/app/globals.css';
+import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 
