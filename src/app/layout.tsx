22fbf05b621e59261952264f0af05c18648ad6f9
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-12-11 02:54:06 +0000

    use these favicons -
    public/images/favicon/android-chrome-192x192.png
    p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased" suppressHydrationWarning>
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/images/favicon/favicon.ico" sizes="any" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
