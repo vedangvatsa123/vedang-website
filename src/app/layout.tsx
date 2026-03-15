e32f0ac5757c54924c1b56f7fe7417a1e8646d83
vedangvatsa123 <vedangvats@gmail.com>
2026-03-16 04:10:33 +0530

    feat: add 120x120 apple-touch-icon link in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -148,6 +148,7 @@ export default function RootLayout({
           }}
         />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/favicon-120x120.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
